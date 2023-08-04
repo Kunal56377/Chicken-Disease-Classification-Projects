b74d0ce2d563060c3a725cc713cf8d36f9580387
Kunal56377 <kunalmugalkhod007@gmail.com>
2023-08-04 13:54:19 +0530

    dvc added

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification-Projects"
-AUTHOR_USER_NAME = "Kunal56377"
+REPO_NAME = "Chicken-Disease-Classification--Project"
+AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "kunalmugalkhod007@gmail.com"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
 
 
 setuptools.setup(
